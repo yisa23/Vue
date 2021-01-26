0679c0802e77caa147868d939ef5c957ba4126ca
Your Name <you@example.com>
2021-01-26 15:09:38 +0800

    02

---
+++ component/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1 v-text="msg"></h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "Hello",
+    };
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  color: blueviolet;
+}
+</style>
---
+++ component/webpack.config.js
@@ -0,0 +1,69 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+
+module.exports = {
+  entry: "./src/main.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "static/js/bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        // exclude: /(node_modules|bower_components)/,
+        include: [path.resolve(__dirname, "src")],
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        // use: ["style-loader", "css-loader"],
+        use: [
+          "vue-style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              esModule: false,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 1024,
+              name: "static/img/[name].[hash:7].[ext]",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new CleanWebpackPlugin(),
+    new VueLoaderPlugin(),
+  ],
+  devServer: {
+    port: 3000,
+    open: true,
+    quiet: true,
+  },
+  //   mode: "development", // production
+};
